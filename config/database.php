f906044ec95170aead9ba5c4ed13641e5651b58b
Inori Yuzuriha <drawangel1234@yahoo.com>
2019-05-02 06:17:36 +0800

    Added JawsDB database configuration

--- config/database.php
+++ config/database.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Support\Str;
 
+$url = parse_url(getenv("JAWSDB_URL")) ?? null;
+
+$host = $url["host"] ?? null;
+$username = $url["user"] ?? null;
+$password = $url["pass"] ?? null;
+$database = substr($url["path"], 1) ?? null;
+
 return [
 
     /*
